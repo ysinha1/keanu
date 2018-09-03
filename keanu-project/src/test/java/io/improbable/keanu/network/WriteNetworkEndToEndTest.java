9c2c162f8dacf43f8d3c7321615d51545e0f33b9
Michael Lewin <michaellewin@improbable.io>
2018-09-03 15:34:05 +0100

    first write an end-to-end test

---
+++ keanu-project/src/test/java/io/improbable/keanu/network/WriteNetworkEndToEndTest.java
@@ -0,0 +1,47 @@
+package io.improbable.keanu.network;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+import io.improbable.keanu.vertices.ConstantVertex;
+import io.improbable.keanu.vertices.dbl.DoubleVertex;
+import io.improbable.keanu.vertices.dbl.nonprobabilistic.ConstantDoubleVertex;
+import io.improbable.keanu.vertices.dbl.probabilistic.GaussianVertex;
+
+public class WriteNetworkEndToEndTest {
+
+    BayesianNetwork network;
+
+    @Before
+    public void createNetwork() {
+        ConstantDoubleVertex mu = ConstantVertex.of(1.);
+        ConstantDoubleVertex sigma = ConstantVertex.of(2.);
+        GaussianVertex gaussianVertex = new GaussianVertex(mu, sigma);
+        GaussianVertex gaussianVertex2 = new GaussianVertex(mu, sigma);
+        DoubleVertex sum = gaussianVertex.plus(gaussianVertex2);
+        network = new BayesianNetwork(sum.getConnectedGraph());
+    }
+
+    @Test
+    public void youCanWriteANetwork() throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        OutputStream outStream = new PrintStream(byteArrayOutputStream, true, "UTF-8");
+        network.write(outStream);
+        String expected = Resources.toString(Resources.getResource("network-example.txt"), Charsets.UTF_8);
+        assertThat(outStream.toString(), equalTo(expected));
+    }
+}
