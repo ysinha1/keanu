65be2b3363752c9f1d00e3b1e9ac5ee6f29e996b
Michael Lewin <michaellewin@improbable.io>
2018-09-04 08:38:42 +0100

    wrote new test youCanWriteAVertexWithItsParentsAsJson

--- keanu-project/src/test/java/io/improbable/keanu/network/WriteVertexTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/network/WriteVertexTest.java
@@ -7,11 +7,13 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import io.improbable.keanu.vertices.ConstantVertex;
 import io.improbable.keanu.vertices.Vertex;
 import io.improbable.keanu.vertices.VertexId;
 import io.improbable.keanu.vertices.dbl.nonprobabilistic.CastDoubleVertex;
@@ -22,13 +24,28 @@ public class WriteVertexTest {
     @Mock
     private OutputStream outputStream;
 
+    @Before
+    public void resetVertexIds() {
+        VertexId.ID_GENERATOR.set(0)x;
+    }
+
     @Test
-    public void youCanWriteAVertexWithItsParents() throws IOException {
+    public void youCanWriteAVertexWithItsParentsAsCsv() throws IOException {
         Vertex parent = mock(Vertex.class);
         when(parent.getId()).thenReturn(new VertexId());
         Vertex vertex = new CastDoubleVertex(parent);
         vertex.write(System.out);
         vertex.write(outputStream);
+        verify(outputStream).write("1|CastDoubleVertex|[0]".getBytes());
+
+    }
+
+    @Test
+    public void youCanWriteAVertexWithItsParentsAsJson() throws IOException {
+        Vertex parent = ConstantVertex.of(0.1);
+        Vertex vertex = new CastDoubleVertex(parent);
+//        vertex.writeAsJson(System.out);
+        vertex.writeAsJson(outputStream);
         verify(outputStream).write("1|CastDoubleVertex|[0]".getBytes());
 
     }
