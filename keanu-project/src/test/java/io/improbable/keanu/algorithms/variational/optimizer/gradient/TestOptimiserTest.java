1c668326fec8c04cdbe4831d7f21bf8959bd3925
Michael Lewin <michaellewin@improbable.io>
2018-10-23 13:48:21 +0100

    added java test

---
+++ keanu-project/src/test/java/io/improbable/keanu/algorithms/variational/optimizer/gradient/TestOptimiserTest.java
@@ -0,0 +1,13 @@
+package io.improbable.keanu.algorithms.variational.optimizer.gradient;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestOptimiserTest {
+
+    @Test
+    public void testOptimiser() {
+        TestOptimiser optimiser = new TestOptimiser(1);
+    }
+}
