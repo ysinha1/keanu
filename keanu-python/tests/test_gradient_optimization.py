8996220ba196ee27e1449ed9c16b34c2b8a3a1df
Charlie Crisp <charliecrisp@improbable.io>
2018-10-24 16:40:00 +0100

    move java_import into test

--- keanu-python/tests/test_gradient_optimization.py
+++ keanu-python/tests/test_gradient_optimization.py
@@ -1,11 +1,16 @@
 from keanu.base import JavaObjectWrapper
+from py4j.java_gateway import java_import
 from keanu.context import KeanuContext
 
 # Minimum repro case for the issue.
 # For reproducing the Windows gradle build issue.
+k = KeanuContext().jvm_view()
+
+java_import(k, "io.improbable.keanu.algorithms.variational.optimizer.gradient.TestOptimiser")
+
 class Thing(JavaObjectWrapper):
     def __init__(self, x):
-        super(Thing, self).__init__(KeanuContext().jvm_view().TestOptimiser, x)
+        super(Thing, self).__init__(k.TestOptimiser, x)
 
 def test_optimiser():
     optimiser = Thing(1)
