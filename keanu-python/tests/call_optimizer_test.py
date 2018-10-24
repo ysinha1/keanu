afeb7de80119c77222b0c449e468bbecfd80809d
Michael Lewin <michaellewin@improbable.io>
2018-10-24 08:46:08 +0100

    added call_optimizer_test so that I can try to repro the issue without using pytest

---
+++ keanu-python/tests/call_optimizer_test.py
@@ -0,0 +1,7 @@
+from test_temp import test_const_takes_ndarray
+from test_temp_2 import test_coalmining
+from test_gradient_optimization import test_optimiser
+
+test_coalmining()
+test_const_takes_ndarray(1)
+test_optimiser()
