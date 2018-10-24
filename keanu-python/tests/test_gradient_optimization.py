b1d64242199af770b6781d69b610a8f097e6b6ac
Charlie Crisp <charliecrisp@improbable.io>
2018-10-24 12:08:10 +0100

    simplified example that causes NSP error

--- keanu-python/tests/test_gradient_optimization.py
+++ keanu-python/tests/test_gradient_optimization.py
@@ -5,62 +5,8 @@ from keanu import x_TestThing
 from py4j.java_gateway import get_field
 from py4j.protocol import Py4JJavaError
 
-
-@pytest.fixture
-def model():
-    model = thermometers.model()
-    model.thermometer_one.observe(22.0)
-    model.thermometer_two.observe(20.0)
-    return model
-
-
-# def test_gradient_op_bayes_net(model):
-#     net = kn.BayesNet(model.temperature.getConnectedGraph())
-#     gradient_optimizer = kn.GradientOptimizer(net)
-#     assert gradient_optimizer.net is net
-#
-#
-# def test_gradient_op_vertex(model):
-#     gradient_optimizer = kn.GradientOptimizer(model.temperature)
-#     assert len(gradient_optimizer.net.getLatentVertices()) == 1
-#
-#
-# def test_gradient_op_throws_with_invalid_net_param():
-#     with pytest.raises(ValueError) as excinfo:
-#         kn.GradientOptimizer(500)
-#
-#
-# def test_gradient_can_set_max_eval_builder_properties(model):
-#     net = kn.BayesNet(model.temperature.getConnectedGraph())
-#     gradient_optimizer = kn.GradientOptimizer(model.temperature, max_evaluations=5)
-#
-#     with pytest.raises(Py4JJavaError) as excinfo:
-#         #This throws a Gradient Optimizer: "Reached Max Evaluations" error
-#         logProb = gradient_optimizer.max_a_posteriori()
-#
-#
-# def test_thermometers_map_gradient(model):
-#     net = kn.BayesNet(model.temperature.getConnectedGraph())
-#     gradient_optimizer = kn.GradientOptimizer(net)
-#     net.unwrap().probeForNonZeroProbability(100)
-#     gradient_optimizer.max_a_posteriori()
-#     # assert logProb < 0.
-#
-#     temperature = model.temperature.getValue().scalar()
-#     assert 20.995 < temperature <  21.005
-
-
 # Minimum repro case for the issue.
 def test_optimiser():
     optimiser = x_TestThing(1)
     # assert False
 
-
-# def test_thermometers_max_likelihood_gradient(model):
-#     net = kn.BayesNet(model.temperature.getConnectedGraph())
-#     gradient_optimizer = kn.GradientOptimizer(net)
-#     logProb = gradient_optimizer.max_likelihood()
-#     assert logProb < 0.
-#
-#     temperature = model.temperature.getValue().scalar()
-#     assert 20.995 < temperature <  21.005
--- keanu-python/tests/test_temp.py
+++ keanu-python/tests/test_temp.py
@@ -3,7 +3,6 @@ from keanu import Const
 import numpy as np
 
 
-@pytest.mark.parametrize("foo", [1])
 def test_const_takes_ndarray(foo):
     ndarray = np.array([[1, 2], [3, 4]])
     v = Const(ndarray)
