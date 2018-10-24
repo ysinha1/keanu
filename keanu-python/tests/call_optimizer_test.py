fcc34d9bb7095592354acfe237f6746371ef2fdd
Charlie Crisp <charliecrisp@improbable.io>
2018-10-24 16:40:57 +0100

    remove coalmining from test

--- keanu-python/tests/call_optimizer_test.py
+++ keanu-python/tests/call_optimizer_test.py
@@ -1,7 +1,5 @@
 from test_temp import test_const_takes_ndarray
-from test_temp_2 import test_coalmining
 from test_gradient_optimization import test_optimiser
 
-test_coalmining()
 test_const_takes_ndarray(1)
 test_optimiser()
