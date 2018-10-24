ff1a73c1a7137c8e440679ef8b9a37384db5ebf2
Charlie Crisp <charliecrisp@improbable.io>
2018-10-24 12:18:09 +0100

    test_gradient_optimization no longer depends on optimizer.py

--- keanu-python/tests/test_gradient_optimization.py
+++ keanu-python/tests/test_gradient_optimization.py
@@ -1,7 +1,13 @@
-from keanu import x_TestThing
+from keanu.base import JavaObjectWrapper
+from keanu.context import KeanuContext
 
 # Minimum repro case for the issue.
+# For reproducing the Windows gradle build issue.
+class Thing(JavaObjectWrapper):
+    def __init__(self, x):
+        super(Thing, self).__init__(KeanuContext().jvm_view().TestOptimiser, x)
+
 def test_optimiser():
-    optimiser = x_TestThing(1)
+    optimiser = Thing(1)
     # assert False
 
