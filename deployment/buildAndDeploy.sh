383166fa7611eeabc0fafc66618ebfd86bd51f74
George Nash <georgenash@improbable.io>
2018-11-07 12:57:10 +0000

    move daemon statement

--- deployment/buildAndDeploy.sh
+++ deployment/buildAndDeploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./gradlew clean build -x :keanu-python:build --daemon uploadArchives -PnexusUser=$NEXUS_USER -PnexusPassword=$NEXUS_PASSWORD -Psigning.keyId=$SIGNING_KEY_ID -Psigning.password=$SIGNING_PASSWORD -Psigning.secretKeyRingFile=../deployment/secret-keys-keanu.gpg --info --stacktrace
+./gradlew --daemon clean build -x :keanu-python:build uploadArchives -PnexusUser=$NEXUS_USER -PnexusPassword=$NEXUS_PASSWORD -Psigning.keyId=$SIGNING_KEY_ID -Psigning.password=$SIGNING_PASSWORD -Psigning.secretKeyRingFile=../deployment/secret-keys-keanu.gpg --info --stacktrace
